7f09c6160cefa22cc56814c6e642a11732c1e91e
Marcelo Moreira <marcelo@scit.com.br>
2021-10-16 22:08:20 +0100

    Cleaning code

--- api/app.py
+++ api/app.py
@@ -7,57 +7,57 @@ import os
 
 from flask_sqlalchemy import SQLAlchemy
 
-#create flask object and set configuration
+# create flask object and set configuration
 app = flask.Flask(__name__)
 
 csrf = CSRFProtect()
-csrf.init_app(app) # Compliant
+csrf.init_app(app)  # Compliant
 
 app.config["DATE_FORMAT"] = "%Y-%m-%d"
 if os.environ.get('SQLALCHEMY_DATABASE_URI'):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    #create SQLAlchemy object
+    # create SQLAlchemy object
     db = SQLAlchemy(app)
 else:
     raise Exception('Please provide SQLALCHEMY_DATABASE_URI environment value')
 
-#db.model class definition for SQLAlchemy - it will create table when called
+# db.model class definition for SQLAlchemy - it will create table when called
 class Users(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    username = db.Column(db.String(80), unique=True, nullable=False)
    birthdate = db.Column(db.Date, nullable=False)
 
-#route for flask method GET path /hello/<username>
+# route for flask method GET path /hello/<username>
 @app.route('/hello/<username>', methods=['GET'])
 def get_hello(username):
-    #check if all symbols are letters, else return error
+    # check if all symbols are letters, else return error
     if username.isalpha():
         try:
-            #query for user by username, if not present user = None
+            # query for user by username, if not present user = None
             user = Users.query.filter_by(username=username).first()
             if(user):
-                    # get today's date
-                    now = datetime.now()
-                    # changing user's birtdate year to this year, in order to compare days
-                    formatted_birthday = datetime(now.year, user.birthdate.month, user.birthdate.day)
-                    # calculating days between user's birtdate and today
-                    days_to_birthday=(formatted_birthday - now.today()).days+1
-                    # if days more than 0 return message with days ti birtdate
-                    if(days_to_birthday>0):
-                        return jsonify(
-                            message="Hello, {}! Your birthday is in {} day(s)".format(username, days_to_birthday)
-                        )
-                    # if days is equal to 0 greet user
-                    elif days_to_birthday == 0:
-                        return jsonify(
-                            message="Hello, {}! Happy birthday!".format(username)
-                        )
-                    # else return message that user's birtdate already passed
-                    else:
-                        return jsonify(
-                            message="Hello, {}! Your birthday has already passed".format(username)
-                        )
+                # get today's date
+                now = datetime.now()
+                # changing user's birtdate year to this year, in order to compare days
+                formatted_birthday = datetime(now.year, user.birthdate.month, user.birthdate.day)
+                # calculating days between user's birtdate and today
+                days_to_birthday=(formatted_birthday - now.today()).days+1
+                # if days more than 0 return message with days ti birtdate
+                if(days_to_birthday>0):
+                    return jsonify(
+                        message="Hello, {}! Your birthday is in {} day(s)".format(username, days_to_birthday)
+                    )
+                # if days is equal to 0 greet user
+                elif days_to_birthday == 0:
+                    return jsonify(
+                        message="Hello, {}! Happy birthday!".format(username)
+                    )
+                # else return message that user's birtdate already passed
+                else:
+                    return jsonify(
+                        message="Hello, {}! Your birthday has already passed".format(username)
+                    )
             # if user = None return error that user doesn't exist with http code = 400 Bad request
             else:
                 return ({"error": "username {} is not presented".format(username)}, 400)
@@ -71,11 +71,11 @@ def get_hello(username):
 
 @app.route('/hello/<username>', methods=['PUT'])
 def put_hello(username):
-    #check if all symbols are letters, else return error
+    # check if all symbols are letters, else return error
     if username.isalpha():
-        #check if Content-type is application/json
+        # check if Content-type is application/json
         if request.headers.get('Content-Type') == 'application/json':
-            #check if request body is json
+            # check if request body is json
             if request.is_json:
                 try:
                     # get dateOfBirth from request body
